1c17d14e5ed2f4fbc8899234064a2b2cccabe3f8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-12 20:21:09 +0000

    Deploy frednordell/nolla.nu-before to github.com/frednordell/nolla.nu-before.git:release

---
+++ precache-manifest.48651e44cda76d57d8726f1474b7bf3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22b9ad0e48802945c408cd2d8ec2a61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6985130b18b79e209fa",
+    "url": "/static/css/main.18cb13e9.chunk.css"
+  },
+  {
+    "revision": "84427b1ed15fa82e881b",
+    "url": "/static/js/2.b6bcd8df.chunk.js"
+  },
+  {
+    "revision": "c6985130b18b79e209fa",
+    "url": "/static/js/main.3cc5186d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
